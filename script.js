98e616b03d1bd6ec6723934be4d7cc7131d69b10
Saloni <pandeysaloni013@gmail.com>
2024-09-01 23:34:25 -0700

    modified file

--- script.js
+++ script.js
@@ -4,14 +4,14 @@
 
 //Tasks / Activities:
 
-//  1. Declare a variable using var , assign it a number, and log the value to the console .
+// 🟢Task 1. Declare a variable using var , assign it a number, and log the value to the console .
 
 var x = 12;
 // var x = 10;
 console.log(x);
 console.log(typeof x);
 
-// 2. Declare a variable using let , assign it a string, and log the value to the console.
+// 🟢Task 2. Declare a variable using let , assign it a string, and log the value to the console.
 let str = "12";
 console.log(str);
 console.log(typeof str);
@@ -27,7 +27,7 @@ let num1 = 12;
 num1 = 21; // here, in let we cannot declare with same name, but can re-assing its value
 console.log(num1);
 
-// 3.  Declare a variable using const, assing it a boolean value, and log the value to the console.
+// 🟢Task 3.  Declare a variable using const, assing it a boolean value, and log the value to the console.
 
 const str1 = false;
 console.log(typeof str1);
@@ -37,7 +37,7 @@ console.log(typeof str2);
 
 // // Activity 3: Data Types;
 
-// 4. Create variables of different data types(number , string, boolean, object, array) and log  each variable's type using the typeof operator.
+// 🟢Task 4. Create variables of different data types(number , string, boolean, object, array) and log  each variable's type using the typeof operator.
 
 let number = 21;
 console.log(number);
@@ -57,12 +57,12 @@ console.log(typeof obj);
 
 let arr = [1, 2, 4, "hello"];
 console.log(arr);
-console.log(typeof Array.isArray(arr));
+console.log(typeof Array.isArray());
 // console.log(typeof arr);
 
 // Activity 4: Reassigning Variables
 
-// 5. Declare a variable using let, assign it an intial value , reassign a new value, and log  both values to the console.
+//🟢Task 5. Declare a variable using let, assign it an intial value , reassign a new value, and log  both values to the console.
 
 let a = 10;
 
@@ -73,7 +73,8 @@ console.log(a);
 // console.log(a);
 
 // Activity 5: Understanding const
-// 6. Try reassigning a varable declared with const and observe the error.
+
+//🟢Task 6. Try reassigning a varable declared with const and observe the error.
 
 const y = 11;
 // y = 12;
